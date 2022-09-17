e05dc9f8cf94a3f50c810b6fec57a69efa2b52cc
David Furtwengler <david.furtwengler@dynetics.com>
2022-09-17 17:54:45 -0500

    Add hw3 part 2 work

---
+++ hw3/sub/2/hw3_2_critical.c
@@ -0,0 +1,17 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[]) {
+  int count = 0;
+  printf("count before parallel: %d, address: %p\n",count, (void*) &count); 
+  #pragma omp parallel
+  {
+    #pragma omp critical
+    {
+      count++;
+      printf("count during parallel: %d, address: %p\n",count, (void*) &count); 
+    }
+  }
+  printf("count after parallel: %d, address: %p\n",count, (void*) &count); 
+}
+
