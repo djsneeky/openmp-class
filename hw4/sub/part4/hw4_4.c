ca9857f69aa76a9963db561e1729c4a805307cda
David Furtwengler <djfurtweng@gmail.com>
2022-09-26 20:02:53 -0500

    Add hw4 part4 and old hw2 slurm outputs

---
+++ hw4/sub/part4/hw4_4.c
@@ -0,0 +1,80 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Q
+{
+  int *q;
+  int pos;
+  int size;
+} Q;
+
+struct Q *initQ(int n)
+{
+  int i;
+  struct Q *newQ = (struct Q *)malloc(sizeof(Q));
+  newQ->q = (int *)malloc(sizeof(int) * n);
+  newQ->pos = -1;
+  newQ->size = n - 1;
+  return newQ;
+}
+
+void putWork(struct Q *workQ)
+{
+  if (workQ->pos < (workQ->size))
+  {
+    workQ->pos++;
+    workQ->q[workQ->pos] = (int)(rand() % 2 * (workQ->pos / 1000));
+  }
+  else
+    printf("ERROR: attempt to add Q element%d\n", workQ->pos + 1);
+}
+
+int getWork(struct Q *workQ)
+{
+  if (workQ->pos > -1)
+  {
+    int w = workQ->q[workQ->pos];
+    workQ->pos--;
+    return w;
+  }
+  else
+    printf("ERROR: attempt to get work from empty Q%d\n", workQ->pos);
+}
+
+int main(int argc, char *argv[])
+{
+  int n = 1000;
+  double start, end;
+
+  struct Q *work_q = initQ(n);
+
+  for (int i = 0; i < n; i++)
+  {
+    putWork(work_q);
+  }
+
+  // sequential work
+  start = omp_get_wtime();
+  for (int i = 0; i < n; i++)
+  {
+    getWork(work_q);
+  }
+  end = omp_get_wtime();
+  printf("Sequential elapsed seconds: %lf\n", end - start);
+
+  // go back to end of queue
+  work_q->pos = work_q->size;
+  
+  // parallel work
+  start = omp_get_wtime();
+#pragma omp parallel for
+  {
+    for (int i = 0; i < n; i++)
+    {
+      getWork(work_q);
+    }
+  }
+  end = omp_get_wtime();
+  printf("Parallel elapsed seconds: %lf\n", end - start);
+}
