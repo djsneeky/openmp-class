0566f1d1c6ee6dd1bedbe16db6e88616b6a6619a
David Furtwengler <david.furtwengler@dynetics.com>
2022-10-31 21:05:11 -0500

    new try at blocks

--- hw8/sub/mm.c
+++ hw8/sub/mm.c
@@ -77,10 +77,14 @@ void initRowColPidMaps( ) {
 }
 
 MPI_Datatype makeType( ) {
-   MPI_Datatype block;
-   MPI_Type_vector(LOCAL_ARRAY_ROWS, LOCAL_ARRAY_COLS, LOCAL_ARRAY_ROWS, MPI_DOUBLE, &block);
+   MPI_Datatype block, blocktype;
+   //MPI_Type_vector(LOCAL_ARRAY_ROWS, LOCAL_ARRAY_COLS, LOCAL_ARRAY_ROWS, MPI_DOUBLE, &block);
+   MPI_Type_vector(LOCAL_ARRAY_ROWS, LOCAL_ARRAY_ROWS, ARRAY_COLS, MPI_INT, &block);
    MPI_Type_commit(&block);
-   return block;
+   MPI_Type_create_resized(block, 0, LOCAL_ARRAY_ROWS*sizeof(double),
+   &blocktype);
+   MPI_Type_commit(&blocktype);
+   return blocktype;
 }
 
 int calculateShift(int current, int shiftAmount, int max) { // shift always up or left
